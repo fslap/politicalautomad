ec4631dbf923371e209aae013276159a640c8084
Florian Leon Steenbuck <admin@kil.ls>
2026-06-12 17:52:16 +0200

    !projectcopy / UI / Party / [+] <MUSTACHE><TWIG><PHP> (add) first stump-shell, templates

---
+++ automad/src/server/Party/RegionScroll/RegionScroll.php
@@ -0,0 +1,64 @@
+<?php
+/*
+ * International Political Party Zone for all Partys also Locally
+ *
+ *                      #;:#;:#;:
+ *                       #;:#;:            #;:#;:#;
+ *            %&                         #;: #;:;:#;:#;:#;:#
+ *    %$%    %$%           #;:    #;:     #;: #;:;:#;:#;:
+ *    ____________                ;:      #;:  #;: #;:#;:#;:#;
+ *   /§§§§§§§§§§§|                                    ;:#;:#;:
+ *   |###########/  / &/      #;:#;:#;:#;:#;:        #;:#;: #;: #;
+ *   /%%%%%%%%%%/   |$$$/        #;:#;:#;:#;:#;#;:#;:;:#;:#;:
+ *  /######### /   |$$$/          #;:#;:#;:#;:#;:#;:#;:#;:#;:#;:
+ * /%%%%%%%%%%/   \CCC/           #;:#;:#;:#;:#;:#;:#;:#;:
+ * \#########|   \\\|      _\    :#;:  :#;:#;:  #;:#;:#;:#;:
+ *  \________/   /[[]]\  \_/   :#\:#\:#\:#\:#;:
+ *   \====__/    \xxxx/              #\:#\:#;:#;:
+ *   |""""""\    |yyy/                #;: #;:#;:#;: |#;:\
+ *   |~~~~~~/ #   |c|                 ;:#;:\;:#;:   |#;:;: 
+ *   +~~~~~/  |   \t/                  #\:#;\       |##;/
+ *    +~~~/   |#                                             
+ *     +~/\       \*+ ~
+ *                      #
+ *
+ *
+ *
+ * https://north.sbdp.ro https://mid.sbdp.ro/ https://pacif.sbdp.ro/ https://low.sbdp.ro/ 
+ * (c) Florian Leon Steenbuck
+ *
+ * Copyright (c) 2026 by Florian Leon Steenbuck
+ * https://kil.ls https://fslap.de 
+ *
+ * See LICENSE_PARTY_PURPOSE.md for license information.
+ */
+namespace Automad\Party\RegionScroll;
+
+defined('AUTOMAD') or die('Direct access not permitted!');
+
+/**
+ * Represents a multi region scroll element that can be used for presenting party locations
+ *
+ * @author Florian Leon Steenbuck
+ * @copyright Copyright (c) 2026 by Florian Leon Steenbuck - https://kil.ls
+ * @license See LICENSE_PARTY_PURPOSE.md for license information
+ *
+ */
+class RegionScroll extends AbstractMultiBlock {
+    public __construct() {
+        parent::__construct(__DIR__, 'regionscroll');
+    }
+
+    public function context(array $config): array
+    {
+         return [
+            'section_id' => $config['section_id'] ?? '',
+            'navigation_target' => $config['navigation_target'] ?? '',
+            'navigation_text' => $config['navigation_text'] ?? '',
+            'scroll_width' => $config['scroll_width'] ?? '',
+            'regions' => $config['regions'] ?? [],
+            'classes' => $this->getClasses(),
+            'has_inner_container' => $this->hasInnerContainer()
+        ];
+    }
+}
