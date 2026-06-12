ac8ae1da9cf2f76911412028e1b68c79589edc8b
Florian Leon Steenbuck <admin@kil.ls>
2026-06-12 18:44:57 +0200

    !projectcopy / UI / Party / [+] <MUSTACHE><TWIG><PHP> (add) first stump-shell, templates

---
+++ automad/src/server/Party/PartyHeader/PartyHeader.php
@@ -0,0 +1,69 @@
+<?php
+/*
+ * International Political Party Zone for all Partys also Locally
+ *
+ *                      #;:#;:#;:
+ *                       #;:#;:            #;:#;:#;
+ *            %&                         #;: #;:;:#;:#;:#;:#
+ *    %$%    %$%           #;:    #;:     #;: #;:;:#;:#;:
+ *    ____________                ;:      #;:  #;: #;:#;:#;:#;
+ *   /§§§§§§§§§§§|                                    ;:#;:#;:
+ *   |###########/  / &/      #;:#;:#;:#;:#;:        #;:#;: #;: #;
+ *   /%%%%%%%%%%/   |$$$/        #;:#;:#;:#;:#;#;:#;:;:#;:#;:
+ *  /######### /   |$$$/          #;:#;:#;:#;:#;:#;:#;:#;:#;:#;:
+ * /%%%%%%%%%%/   \CCC/           #;:#;:#;:#;:#;:#;:#;:#;:
+ * \#########|   \\\|      _\    :#;:  :#;:#;:  #;:#;:#;:#;:
+ *  \________/   /[[]]\  \_/   :#\:#\:#\:#\:#;:
+ *   \====__/    \xxxx/              #\:#\:#;:#;:
+ *   |""""""\    |yyy/                #;: #;:#;:#;: |#;:\
+ *   |~~~~~~/ #   |c|                 ;:#;:\;:#;:   |#;:;: 
+ *   +~~~~~/  |   \t/                  #\:#;\       |##;/
+ *    +~~~/   |#                                             
+ *     +~/\       \*+ ~
+ *                      #
+ *
+ *
+ *
+ * https://north.sbdp.ro https://mid.sbdp.ro/ https://pacif.sbdp.ro/ https://low.sbdp.ro/ 
+ * (c) Florian Leon Steenbuck
+ *
+ * Copyright (c) 2026 by Florian Leon Steenbuck
+ * https://kil.ls https://fslap.de 
+ *
+ * See LICENSE_PARTY_PURPOSE.md for license information.
+ */
+namespace Automad\Party\PartyHeader;
+
+use Automad\Blocks\AbstractDynamicTemplateBlock;
+use Automad\Party\Traits\ComponentConfig;
+
+defined('AUTOMAD') or die('Direct access not permitted!');
+
+/**
+ * Represents a quick statement on the political party side
+ *
+ * @author Florian Leon Steenbuck
+ * @copyright Copyright (c) 2026 by Florian Leon Steenbuck - https://kil.ls
+ * @license See LICENSE_PARTY_PURPOSE.md for license information
+ *
+ */
+class PartyHeader extends AbstractDynamicTemplateBlock {
+	use ComponentConfig;
+    public function __construct() {
+        parent::__construct(__DIR__, 'partyheader');
+    }
+
+    public function context(array $config): array
+    {
+        $party_name_parts = explode(' ', $config['party_name'] ?? '');
+        
+        return [
+            'warning_text' => $config['warning_text'] ?? '',
+            'party_name_parts' => $party_name_parts,
+            'main_link' => $config['main_link'] ?? '',
+            'main_link_text' => $config['main_link_text'] ?? '',
+            'intro_text' => $config['intro_text'] ?? '',
+            'classes' => $this->getClasses()
+        ];
+    }
+}
