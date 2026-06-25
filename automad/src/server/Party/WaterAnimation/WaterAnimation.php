72aba6623296ae86343856013a4a872a640ef377
Florian Leon Steenbuck <florian@kil.ls>
2026-06-25 19:10:59 +0200

    *!grok / Party / WaterAnimation / [+] <PHP> (add) water animation block, (add) party banner

---
+++ automad/src/server/Party/WaterAnimation/WaterAnimation.php
@@ -0,0 +1,67 @@
+<?php
+/*
+ * International Political Party Zone for all Partys also Locally
+ *
+ *                      #;:#;:#;:
+ *                       #;:#;:            #;:#;:#;
+ *            %&                         #;: #;:;:#;:#;:#;:#
+ *    %$%    %$%           #;:    #;:     #;: #;:;:#;:#;:
+ *    ____________                ;:      #;:  #;: #;:#;:#;:#;
+ *   /§§§§§§§§§§§|                                    ;:#;:#;:
+ *   |###########/  / &/      #;:#;:#;:#;:#;:        #;:#;: #;: #;
+ *   /%%%%%%%%%%/   |$$$/        #;:#;:#;:#;:#;#;:#;:;:#;:#;:
+ *  /######### /   |$$$/          #;:#;:#;:#;:#;:#;:#;:#;:#;:#;:
+ * /%%%%%%%%%%/   \CCC/           #;:#;:#;:#;:#;:#;:#;:#;:
+ * \#########|   \\|      _\    :#;:  :#;:#;:  #;:#;:#;:#;:
+ *  \________/   /[[]]\  \_/   :#\:#\:#\:#\:#;:
+ *   \====__/    \xxxx/              #\:#\:#;:#;:
+ *   |""""""\    |yyy/                #;: #;:#;:#;: |#;:\
+ *   |~~~~~~/ #   |c|                 ;:#;:\;:#;:   |#;:;:
+ *   +~~~~~/  |   \t/                  #\:#;\       |##;/
+ *    +~~~/   |#
+ *     +~/\       \*+ ~
+ *                      #
+ *
+ *
+ *
+ * https://north.sbdp.ro https://mid.sbdp.ro/ https://pacif.sbdp.ro/ https://low.sbdp.ro/
+ * (c) Florian Leon Steenbuck
+ *
+ * Copyright (c) 2026 by Florian Leon Steenbuck
+ * https://kil.ls https://fslap.de
+ *
+ * See LICENSE_PARTY_PURPOSE.md for license information.
+ */
+namespace Automad\Party\WaterAnimation;
+
+use Automad\Blocks\AbstractDynamicTemplateBlock;
+use Automad\Party\Traits\ComponentConfig;
+
+defined('AUTOMAD') or die('Direct access not permitted!');
+
+/**
+ * Party component Water Animation (politicalpartysite → Automad Party block).
+ *
+ * @author Florian Leon Steenbuck
+ * @copyright Copyright (c) 2026 by Florian Leon Steenbuck - https://kil.ls
+ * @license See LICENSE_PARTY_PURPOSE.md for license information
+ *
+ */
+class WaterAnimation extends AbstractDynamicTemplateBlock {
+	use ComponentConfig;
+
+	public function __construct() {
+		parent::__construct(__DIR__, 'wateranimation');
+	}
+
+	public function context(array $config): array
+	{
+
+        return [
+            'layers' => $config['layers'] ?? [],
+            'classes' => $this->getClasses($config),
+            'mobile' => false // Could be passed from context
+        ];
+	}
+}
+
