c6f7431b71aa059f9a917558ccdfd0665bebad70
Florian Leon Steenbuck <florian@kil.ls>
2026-06-25 19:13:03 +0200

    *!grok / Party / WpHeading / [+] <PHP> (add) wp heading block, (add) party banner

---
+++ automad/src/server/Party/WpHeading/WpHeading.php
@@ -0,0 +1,80 @@
+<?php
+/*
+ * International Political Party Zone for all Partys also Locally
+ *
+ *                      #;:#;:#;:
+ *                       #;:#;:            #;:#;:#;
+ *            %&                         #;: #;:;:#;:#;:#;:#
+ *    %$%    %$%           #;:    #;:     #;: #;:;:#;:#;:
+ *    ____________                ;:      #;:  #;: #;:#;:#;:#;
+ *   /§§§§§§§§§§§|                                    ;:#;:#;:
+ *   |###########/  / &/      #;:#;:#;:#;:#;:        #;:#;: #;: #;
+ *   /%%%%%%%%%%/   |$$$/        #;:#;:#;:#;:#;#;:#;:;:#;:#;:
+ *  /######### /   |$$$/          #;:#;:#;:#;:#;:#;:#;:#;:#;:#;:
+ * /%%%%%%%%%%/   \CCC/           #;:#;:#;:#;:#;:#;:#;:#;:
+ * \#########|   \\|      _\    :#;:  :#;:#;:  #;:#;:#;:#;:
+ *  \________/   /[[]]\  \_/   :#\:#\:#\:#\:#;:
+ *   \====__/    \xxxx/              #\:#\:#;:#;:
+ *   |""""""\    |yyy/                #;: #;:#;:#;: |#;:\
+ *   |~~~~~~/ #   |c|                 ;:#;:\;:#;:   |#;:;:
+ *   +~~~~~/  |   \t/                  #\:#;\       |##;/
+ *    +~~~/   |#
+ *     +~/\       \*+ ~
+ *                      #
+ *
+ *
+ *
+ * https://north.sbdp.ro https://mid.sbdp.ro/ https://pacif.sbdp.ro/ https://low.sbdp.ro/
+ * (c) Florian Leon Steenbuck
+ *
+ * Copyright (c) 2026 by Florian Leon Steenbuck
+ * https://kil.ls https://fslap.de
+ *
+ * See LICENSE_PARTY_PURPOSE.md for license information.
+ */
+namespace Automad\Party\WpHeading;
+
+use Automad\Blocks\AbstractDynamicTemplateBlock;
+use Automad\Party\Traits\ComponentConfig;
+
+defined('AUTOMAD') or die('Direct access not permitted!');
+
+/**
+ * Party component Wp Heading (politicalpartysite → Automad Party block).
+ *
+ * @author Florian Leon Steenbuck
+ * @copyright Copyright (c) 2026 by Florian Leon Steenbuck - https://kil.ls
+ * @license See LICENSE_PARTY_PURPOSE.md for license information
+ *
+ */
+class WpHeading extends AbstractDynamicTemplateBlock {
+	use ComponentConfig;
+
+	public function __construct() {
+		parent::__construct(__DIR__, 'wpheading');
+	}
+
+	public function context(array $config): array
+	{
+
+        $level = strtolower($config['level'] ?? 'h2');
+        $allowed = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
+        if (!in_array($level, $allowed, true)) {
+            $level = 'h2';
+        }
+
+        $text = $config['text'] ?? '';
+        $classes = $this->getClasses($config);
+        $class_attr = $classes ? ' class="' . $classes . '"' : '';
+
+        return [
+            'level' => $level,
+            'text' => $text,
+            'classes' => $classes,
+            'html' => '<' . $level . $class_attr . '>' .
+                htmlspecialchars($text, ENT_QUOTES, 'UTF-8') .
+                '</' . $level . '>'
+        ];
+	}
+}
+
