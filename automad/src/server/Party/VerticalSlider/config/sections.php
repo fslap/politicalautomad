68dca0a311e5abb1c1bb39c766549692f99246f4
Florian Leon Steenbuck <florian@kil.ls>
2026-06-25 19:08:33 +0200

    *!grok / Party / VerticalSlider / [+] <CONFIG> (add) sections config

---
+++ automad/src/server/Party/VerticalSlider/config/sections.php
@@ -0,0 +1,265 @@
+<?php
+
+return array (
+  0 => 
+  array (
+    'type' => 'party-header',
+    'component' => 'PartyHeader',
+    'config' => 'party-header.php',
+    'template_twig' => 'components/twig/party-header.twig',
+    'template_mustache' => 'components/mustache/party-header.mustache',
+    'engine' => 'auto',
+  ),
+  1 => 
+  array (
+    'type' => 'region-scroll',
+    'component' => 'RegionScroll',
+    'config' => 'region-scroll.php',
+    'template_twig' => 'components/twig/region-scroll.twig',
+    'template_mustache' => 'components/mustache/region-scroll.mustache',
+    'engine' => 'auto',
+  ),
+  2 => 
+  array (
+    'type' => 'contact-closer',
+    'component' => 'ContactCloser',
+    'config' => 'contact-closer.php',
+    'template_twig' => 'components/twig/contact-closer.twig',
+    'template_mustache' => 'components/mustache/contact-closer.mustache',
+    'engine' => 'auto',
+  ),
+  3 => 
+  array (
+    'type' => 'water-animation',
+    'component' => 'WaterAnimation',
+    'config' => 'water-animation.php',
+    'template_twig' => 'components/twig/water-animation.twig',
+    'template_mustache' => 'components/mustache/water-animation.mustache',
+    'engine' => 'auto',
+  ),
+  4 => 
+  array (
+    'type' => 'security-concept',
+    'component' => 'SecurityConcept',
+    'config' => 'ticket.php',
+    'template_twig' => 'components/twig/security-concept.twig',
+    'template_mustache' => 'components/mustache/security-concept.mustache',
+    'engine' => 'auto',
+  ),
+  5 => 
+  array (
+    'type' => 'documents',
+    'component' => 'Documents',
+    'config' => 'unfertig.php',
+    'template_twig' => 'components/twig/documents.twig',
+    'template_mustache' => 'components/mustache/documents.mustache',
+    'engine' => 'auto',
+  ),
+  6 => 
+  array (
+    'type' => 'security-concept',
+    'component' => 'SecurityConcept',
+    'config' => 'ticket.php',
+    'template_twig' => 'components/twig/security-concept.twig',
+    'template_mustache' => 'components/mustache/security-concept.mustache',
+    'engine' => 'auto',
+  ),
+  7 => 
+  array (
+    'type' => 'art-motivation',
+    'component' => 'ArtMotivation',
+    'config' => 'motivate.php',
+    'template_twig' => 'components/twig/art-motivation.twig',
+    'template_mustache' => 'components/mustache/art-motivation.mustache',
+    'engine' => 'auto',
+  ),
+  8 => 
+  array (
+    'type' => 'form',
+    'component' => 'Form',
+    'config' => 'membership-form.php',
+    'template_twig' => 'components/twig/form-section.twig',
+    'template_mustache' => 'components/mustache/form-section.mustache',
+    'engine' => 'auto',
+  ),
+  9 => 
+  array (
+    'type' => 'split',
+    'component' => 'Split',
+    'config' => 'detrain-map.php',
+    'template_twig' => 'components/twig/split-section.twig',
+    'template_mustache' => 'components/mustache/split-section.mustache',
+    'engine' => 'auto',
+  ),
+  10 => 
+  array (
+    'type' => 'markdown',
+    'component' => 'Markdown',
+    'config' => 'theater.php',
+    'template_twig' => 'components/twig/markdown-section.twig',
+    'template_mustache' => 'components/mustache/markdown-section.mustache',
+    'engine' => 'auto',
+  ),
+  11 => 
+  array (
+    'type' => 'quote',
+    'component' => 'Quote',
+    'config' => 'principles.php',
+    'template_twig' => 'components/twig/quote-section.twig',
+    'template_mustache' => 'components/mustache/quote-section.mustache',
+    'engine' => 'auto',
+  ),
+  12 => 
+  array (
+    'type' => 'markdown',
+    'component' => 'Markdown',
+    'config' => 'philosophy.php',
+    'template_twig' => 'components/twig/markdown-section.twig',
+    'template_mustache' => 'components/mustache/markdown-section.mustache',
+    'engine' => 'auto',
+  ),
+  13 => 
+  array (
+    'type' => 'split',
+    'component' => 'Split',
+    'config' => 'bildung.php',
+    'template_twig' => 'components/twig/split-section.twig',
+    'template_mustache' => 'components/mustache/split-section.mustache',
+    'engine' => 'auto',
+  ),
+  14 => 
+  array (
+    'type' => 'split',
+    'component' => 'Split',
+    'config' => 'ministerien-reform.php',
+    'template_twig' => 'components/twig/split-section.twig',
+    'template_mustache' => 'components/mustache/split-section.mustache',
+    'engine' => 'auto',
+  ),
+  15 => 
+  array (
+    'type' => 'security-concept',
+    'component' => 'SecurityConcept',
+    'config' => 'rechtssystem.php',
+    'template_twig' => 'components/twig/security-concept.twig',
+    'template_mustache' => 'components/mustache/security-concept.mustache',
+    'engine' => 'auto',
+  ),
+  16 => 
+  array (
+    'type' => 'security-concept',
+    'component' => 'SecurityConcept',
+    'config' => 'ticket.php',
+    'template_twig' => 'components/twig/security-concept.twig',
+    'template_mustache' => 'components/mustache/security-concept.mustache',
+    'engine' => 'auto',
+  ),
+  17 => 
+  array (
+    'type' => 'split',
+    'component' => 'Split',
+    'config' => 'military-map.php',
+    'template_twig' => 'components/twig/split-section.twig',
+    'template_mustache' => 'components/mustache/split-section.mustache',
+    'engine' => 'auto',
+  ),
+  18 => 
+  array (
+    'type' => 'split',
+    'component' => 'Split',
+    'config' => 'demokratie-reformen.php',
+    'template_twig' => 'components/twig/split-section.twig',
+    'template_mustache' => 'components/mustache/split-section.mustache',
+    'engine' => 'auto',
+  ),
+  19 => 
+  array (
+    'type' => 'markdown',
+    'component' => 'Markdown',
+    'config' => 'soziales.php',
+    'template_twig' => 'components/twig/markdown-section.twig',
+    'template_mustache' => 'components/mustache/markdown-section.mustache',
+    'engine' => 'auto',
+  ),
+  20 => 
+  array (
+    'type' => 'markdown',
+    'component' => 'Markdown',
+    'config' => 'gewerbe-intro.php',
+    'template_twig' => 'components/twig/markdown-section.twig',
+    'template_mustache' => 'components/mustache/markdown-section.mustache',
+    'engine' => 'auto',
+  ),
+  21 => 
+  array (
+    'type' => 'split',
+    'component' => 'Split',
+    'config' => 'wirtschaft.php',
+    'template_twig' => 'components/twig/split-section.twig',
+    'template_mustache' => 'components/mustache/split-section.mustache',
+    'engine' => 'auto',
+  ),
+  22 => 
+  array (
+    'type' => 'markdown',
+    'component' => 'Markdown',
+    'config' => 'spezialwirtschaft.php',
+    'template_twig' => 'components/twig/markdown-section.twig',
+    'template_mustache' => 'components/mustache/markdown-section.mustache',
+    'engine' => 'auto',
+  ),
+  23 => 
+  array (
+    'type' => 'security-concept',
+    'component' => 'SecurityConcept',
+    'config' => 'landwirtschaft-forst.php',
+    'template_twig' => 'components/twig/security-concept.twig',
+    'template_mustache' => 'components/mustache/security-concept.mustache',
+    'engine' => 'auto',
+  ),
+  24 => 
+  array (
+    'type' => 'landscape-scene',
+    'component' => 'LandscapeScene',
+    'config' => 'landscape-scene.php',
+    'template_twig' => 'components/twig/landscape-scene.twig',
+    'template_mustache' => 'components/mustache/landscape-scene.mustache',
+    'engine' => 'auto',
+  ),
+  25 => 
+  array (
+    'type' => 'music-anchor',
+    'component' => 'MusicAnchor',
+    'config' => 'music-anchor.php',
+    'template_twig' => 'components/twig/music-anchor.twig',
+    'template_mustache' => 'components/mustache/music-anchor.mustache',
+    'engine' => 'auto',
+  ),
+  26 => 
+  array (
+    'type' => 'quote',
+    'component' => 'Quote',
+    'config' => 'principle.php',
+    'template_twig' => 'components/twig/quote-section.twig',
+    'template_mustache' => 'components/mustache/quote-section.mustache',
+    'engine' => 'auto',
+  ),
+  27 => 
+  array (
+    'type' => 'music-player',
+    'component' => 'MusicPlayer',
+    'config' => 'economy-music.php',
+    'template_twig' => 'components/twig/music-player.twig',
+    'template_mustache' => 'components/mustache/music-player.mustache',
+    'engine' => 'auto',
+  ),
+  28 => 
+  array (
+    'type' => 'music-player',
+    'component' => 'MusicPlayer',
+    'config' => 'matching-music.php',
+    'template_twig' => 'components/twig/music-player.twig',
+    'template_mustache' => 'components/mustache/music-player.mustache',
+    'engine' => 'auto',
+  ),
+);
