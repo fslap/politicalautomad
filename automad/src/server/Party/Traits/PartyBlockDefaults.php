3276ad364a9fdb174df495f404926c863f6a1fd3
Florian Leon Steenbuck <florian@kil.ls>
2026-06-25 19:20:57 +0200

    *!grok / Party / Traits / [+] <PHP> (add) ComponentConfig, (add) PartyBlockDefaults, (add) party banner

---
+++ automad/src/server/Party/Traits/PartyBlockDefaults.php
@@ -0,0 +1,63 @@
+<?php
+/*
+ * International Political Party Zone for all Partys also Locally
+ *
+ *                      #;:#;:#;:
+ *                       #;:#;:            #;:#;:#;
+ *            %&                         #;: #;:;:#;:#;:#;:#
+ *    %$%    %$%           #;:    #;:     #;: #;:;:#;:#;:
+ *    ____________                ;:      #;:  #;: #;:#;:#;:#;
+ *   /§§§§§§§§§§§|                                    ;:#;:#;:
+ *   |###########/  / &/      #;:#;:#;:#;:#;:        #;:#;: #;: #;
+ *   /%%%%%%%%%%/   |$$$/        #;:#;:#;:#;:#;#;:#;:;:#;:#;:
+ *  /######### /   |$$$/          #;:#;:#;:#;:#;:#;:#;:#;:#;:#;:
+ * /%%%%%%%%%%/   \CCC/           #;:#;:#;:#;:#;:#;:#;:#;:
+ * \#########|   \\\|      _\    :#;:  :#;:#;:  #;:#;:#;:#;:
+ *  \________/   /[[]]\  \_/   :#\:#\:#\:#\:#;:
+ *   \====__/    \xxxx/              #\:#\:#;:#;:
+ *   |""""""\    |yyy/                #;: #;:#;:#;: |#;:\
+ *   |~~~~~~/ #   |c|                 ;:#;:\;:#;:   |#;:;:
+ *   +~~~~~/  |   \t/                  #\:#;\       |##;/
+ *    +~~~/   |#
+ *     +~/\       \*+ ~
+ *                      #
+ *
+ *
+ *
+ * https://north.sbdp.ro https://mid.sbdp.ro/ https://pacif.sbdp.ro/ https://low.sbdp.ro/
+ * (c) Florian Leon Steenbuck
+ *
+ * Copyright (c) 2026 by Florian Leon Steenbuck
+ * https://kil.ls https://fslap.de
+ *
+ * See LICENSE_PARTY_PURPOSE.md for license information.
+ */
+namespace Automad\Party\Traits;
+
+use Automad\Models\ComponentCollection;
+
+defined('AUTOMAD') or die('Direct access not permitted!');
+
+/**
+ * Default replace/toString for party template blocks.
+ *
+ * @author Florian Leon Steenbuck
+ * @copyright Copyright (c) 2026 by Florian Leon Steenbuck - https://kil.ls
+ * @license See LICENSE_PARTY_PURPOSE.md for license information
+ *
+ */
+trait PartyBlockDefaults {
+	public function replace(
+		array $block,
+		?ComponentCollection $ComponentCollection,
+		string $searchRegex,
+		string $replace,
+		bool $replaceInPublishedComponent
+	): array {
+		return $block;
+	}
+
+	public function toString(array $block, ?ComponentCollection $ComponentCollection): string {
+		return (string) json_encode($block['data'] ?? [], JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+	}
+}
