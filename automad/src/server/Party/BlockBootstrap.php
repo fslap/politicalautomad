371e709526ef6dbae99e4f3bb1ce0b674704d16f
Florian Leon Steenbuck <florian@kil.ls>
2026-06-25 19:20:55 +0200

    *!grok / Party / BlockBootstrap / [+] <PHP> (add) dynamic block bootstrap, (add) party banner

---
+++ automad/src/server/Party/BlockBootstrap.php
@@ -0,0 +1,70 @@
+<?php
+/*
+ * International Political Party Zone for all Partys also Locally
+ *
+ *                      #;:#;:#;:
+ *                       #;:#;:            #;:#;:#;
+ *            %&                         #;: #;:;:#;:#;:#;:#
+ *    %$%    %$%           #;:    #;:     #;: #;:;:#;:#;:
+ *    ____________                ;:      #;:  #;: #;:#;:#;:#;
+ *   /§§§§§§§§§§§|                                    ;:#;:#;:
+ *   |###########/  / &/      #;:#;:#;:#;:#;:        #;:#;: #;: #;
+ *   /%%%%%%%%%%/   |$$$/        #;:#;:#;:#;:#;#;:#;:;:#;:#;:
+ *  /######### /   |$$$/          #;:#;:#;:#;:#;:#;:#;:#;:#;:#;:
+ * /%%%%%%%%%%/   \CCC/           #;:#;:#;:#;:#;:#;:#;:#;:
+ * \#########|   \\|      _\    :#;:  :#;:#;:  #;:#;:#;:#;:
+ *  \________/   /[[]]\  \_/   :#\:#\:#\:#\:#;:
+ *   \====__/    \xxxx/              #\:#\:#;:#;:
+ *   |""""""\    |yyy/                #;: #;:#;:#;: |#;:\
+ *   |~~~~~~/ #   |c|                 ;:#;:\;:#;:   |#;:;:
+ *   +~~~~~/  |   \t/                  #\:#;\       |##;/
+ *    +~~~/   |#
+ *     +~/\       \*+ ~
+ *                      #
+ *
+ *
+ *
+ * https://north.sbdp.ro https://mid.sbdp.ro/ https://pacif.sbdp.ro/ https://low.sbdp.ro/
+ * (c) Florian Leon Steenbuck
+ *
+ * Copyright (c) 2026 by Florian Leon Steenbuck
+ * https://kil.ls https://fslap.de
+ *
+ * See LICENSE_PARTY_PURPOSE.md for license information.
+ */
+namespace Automad\Party;
+
+use Automad\Core\Blocks;
+use Automad\DynamicBlockRegister;
+
+defined('AUTOMAD') or die('Direct access not permitted!');
+
+/**
+ * Bootstrap party dynamic blocks into the core Blocks resolver (v2-political).
+ *
+ * @author Florian Leon Steenbuck
+ * @copyright Copyright (c) 2026 by Florian Leon Steenbuck - https://kil.ls
+ * @license See LICENSE_PARTY_PURPOSE.md for license information
+ *
+ */
+final class BlockBootstrap {
+	public static function register(DynamicBlockRegister $register): void {
+		foreach (All::$components as $class) {
+			$register->register(new $class());
+		}
+	}
+
+	public static function createRegister(): DynamicBlockRegister {
+		$register = new DynamicBlockRegister();
+		self::register($register);
+
+		return $register;
+	}
+
+	public static function init(): DynamicBlockRegister {
+		$register = self::createRegister();
+		Blocks::setDynamicRegister($register);
+
+		return $register;
+	}
+}
