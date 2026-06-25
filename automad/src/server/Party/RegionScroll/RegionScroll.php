edafceac7580dc9fc038d8540244f0f2592a6b1e
Florian Leon Steenbuck <florian@kil.ls>
2026-06-25 19:03:25 +0200

    *!grok / Party / RegionScroll / [+] <PHP> (add) region scroll block, (add) party banner

---
+++ automad/src/server/Party/RegionScroll/RegionScroll.php
@@ -0,0 +1,71 @@
+<?php
+/*
+ * International Political Party Zone for all Partys also Locally
+ *
+ *                      #;:#;:#;:
+ *                       #;:#;:            #;:#;:#;
+ *            %&                         #;: #;:;:#;:#;:#;:#
+ *    %$%    %$%           #;:    #;:     #;: #;:;:#;:#;:
+ *    ____________                ;:      #;:  #;: #;:#;:#;:#;
+ *   /§§§§§§§§§§§|                                    ;:#;:#;:
+ *   |###########/  / &/      #;:#;:#;:#;:#;:        #;:#;: #;: #;
+ *   /%%%%%%%%%%/   |$$$/        #;:#;:#;:#;:#;#;:#;:;:#;:#;:
+ *  /######### /   |$$$/          #;:#;:#;:#;:#;:#;:#;:#;:#;:#;:
+ * /%%%%%%%%%%/   \CCC/           #;:#;:#;:#;:#;:#;:#;:#;:
+ * \#########|   \\|      _\    :#;:  :#;:#;:  #;:#;:#;:#;:
+ *  \________/   /[[]]\  \_/   :#\:#\:#\:#\:#;:
+ *   \====__/    \xxxx/              #\:#\:#;:#;:
+ *   |""""""\    |yyy/                #;: #;:#;:#;: |#;:\
+ *   |~~~~~~/ #   |c|                 ;:#;:\;:#;:   |#;:;:
+ *   +~~~~~/  |   \t/                  #\:#;\       |##;/
+ *    +~~~/   |#
+ *     +~/\       \*+ ~
+ *                      #
+ *
+ *
+ *
+ * https://north.sbdp.ro https://mid.sbdp.ro/ https://pacif.sbdp.ro/ https://low.sbdp.ro/
+ * (c) Florian Leon Steenbuck
+ *
+ * Copyright (c) 2026 by Florian Leon Steenbuck
+ * https://kil.ls https://fslap.de
+ *
+ * See LICENSE_PARTY_PURPOSE.md for license information.
+ */
+namespace Automad\Party\RegionScroll;
+
+use Automad\Blocks\AbstractDynamicTemplateBlock;
+use Automad\Party\Traits\ComponentConfig;
+
+defined('AUTOMAD') or die('Direct access not permitted!');
+
+/**
+ * Party component Region Scroll (politicalpartysite → Automad Party block).
+ *
+ * @author Florian Leon Steenbuck
+ * @copyright Copyright (c) 2026 by Florian Leon Steenbuck - https://kil.ls
+ * @license See LICENSE_PARTY_PURPOSE.md for license information
+ *
+ */
+class RegionScroll extends AbstractDynamicTemplateBlock {
+	use ComponentConfig;
+
+	public function __construct() {
+		parent::__construct(__DIR__, 'regionscroll');
+	}
+
+	public function context(array $config): array
+	{
+
+        return [
+            'section_id' => $config['section_id'] ?? '',
+            'navigation_target' => $config['navigation_target'] ?? '',
+            'navigation_text' => $config['navigation_text'] ?? '',
+            'scroll_width' => $config['scroll_width'] ?? '',
+            'regions' => $config['regions'] ?? [],
+            'classes' => $this->getClasses($config),
+            'has_inner_container' => $this->hasInnerContainer($config)
+        ];
+	}
+}
+
